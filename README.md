bc9bbda8c5df97113ee44266101cfc38742b248f
Lucas deGomez <lucasdegomez@gmail.com>
2015-06-21 09:48:20 +0200

    Creditos
    Rama con los créditos (y algunas otras modificaciones de funcionalidad)

---
+++ README.md
@@ -0,0 +1,3 @@
+A project to test nodeJS skills following the instructions from
+
+https://github.com/jquemada/quiz-2015
--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET author */
+router.get('/author', function(req, res, next) {
+  res.render('author', { author: 'Lucas deGomez'});
+});
+
+
 /* GET question and answer */
 router.get('/quizzes/question', quizController.question);
 router.get('/quizzes/answer', quizController.answer);
+
 
 module.exports = router;
