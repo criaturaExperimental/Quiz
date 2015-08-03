12dbcc63ca7c63b006cd3c720c36f77197d93321
Lucas deGomez <lucasdegomez@gmail.com>
2015-08-03 18:41:36 +0200

    Categorias 2.0
    Nueva adaptación para añadir categorias, la base de datos sigue sin
    poder accederse

--- models/models.js
+++ models/models.js
@@ -39,10 +39,12 @@ sequelize.sync().then(function() {
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.create({pregunta: '¿Cuál es la capital de Italia?',
-             respuesta: 'Roma'
+             respuesta: 'Roma',
+             tema: 'Humanidades'
             });
       Quiz.create({pregunta: '¿Cuál es la capital de Portugal?',
-             respuesta: 'Lisboa'
+             respuesta: 'Lisboa',
+             tema: 'Humanidades'
             }).then(function(){ console.log('Base de datos inicializada') });
     }
   });
--- models/quiz.js
+++ models/quiz.js
@@ -2,16 +2,20 @@
 // Definicion del modelo de Quiz con validación
 
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define(
-  	'Quiz',
-    { pregunta: {
-        type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Pregunta"}}
-      },
-      respuesta: {
-        type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Respuesta"}}
-      }
-    }
-  );
+	return sequelize.define(
+		'Quiz',
+		{	pregunta: {
+			   type: DataTypes.STRING,
+			   validate: { notEmpty: {msg: "--> Falta pregunta"}}
+			},
+			respuesta: {
+				type: DataTypes.STRING,
+				validate: {notEmpty: {msg: "--> Falta respuesta"}}
+			},
+			tema:{
+				type: DataTypes.STRING,
+				validate: {notEmpty: {msg: "--> Falta temática de pregunta"}}
+			}
+		}
+	);
 }
